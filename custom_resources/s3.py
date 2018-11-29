295479c13c56606a98a18af821c856bcf29ef098
Niels Laukens <niels@dest-unreach.be>
2018-11-29 11:50:30 +0100

    Add s3.object resource

---
+++ custom_resources/s3.py
@@ -0,0 +1,26 @@
+from six import string_types
+
+from .LambdaBackedCustomResource import LambdaBackedCustomResource
+
+
+class Object(LambdaBackedCustomResource):
+    props = {
+        'Region': (string_types, False),  # Default: current region
+        'Bucket': (string_types, True),  # Bucket name
+        'Key': (string_types, True),  # Location within bucket
+        'Body': (object, False),  # string, or JSON-able content. Default: empty file
+    }
+
+    @classmethod
+    def _lambda_policy(cls):
+        return {
+            "Version": "2012-10-17",
+            "Statement": [{
+                "Effect": "Allow",
+                "Action": [
+                    "s3:PutObject",
+                    "s3:DeleteObject",
+                ],
+                "Resource": "*",
+            }],
+        }
