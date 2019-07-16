3767ad4d6c17654ab9e7b4ceedafc8068c9ed2a5
Rob Kenis <rob.kenis@vrt.be>
2019-07-16 13:53:22 +0200

    Add custom-resource to update beanstalk tags
    NOTE: This is a dirty hack to update tags on beanstalk. Once aws finally
    allows updating tags using CloudFormation, remove this resource.

---
+++ lambda_code/elasticbeanstalk/Tags/index.py
@@ -0,0 +1,32 @@
+from cfn_custom_resource import CloudFormationCustomResource
+
+
+class Tags(CloudFormationCustomResource):
+    def validate(self):
+        self.environmentArn = self.resource_properties.get('EnvironmentArn')
+        self.tags = self.resource_properties.get('Tags')
+
+    @staticmethod
+    def tags_to_update(tags):
+        return list(map(lambda tag: {'Key': tag[0], 'Value': tag[1]}, tags.items()))
+
+    def update_tags(self):
+        client = self.get_boto3_session().client('elasticbeanstalk')
+        client.update_tags_for_resource(
+            ResourceArn=self.environmentArn,
+            TagsToAdd=self.tags_to_update(self.tags)
+        )
+        return {'TagsToUpdate': self.tags_to_update(self.tags)}
+
+    def create(self):
+        return self.update_tags()
+
+    def update(self):
+        return self.update_tags()
+
+    def delete(self):
+        # Deleting not supported for now. Tags will disappear when environment is deleted.
+        pass
+
+
+handler = Tags.get_handler()
---
+++ lambda_code/elasticbeanstalk/Tags/test/test_index.py
@@ -0,0 +1,18 @@
+# from lambda_code.elasticbeanstalk.Tags.index import Tags
+from ..index import Tags
+
+
+def test_tags_dict_to_list():
+    tags_to_add = Tags.tags_to_update({'LabelOwner': 'kenisro', 'LabelProject': 'video-monitoring-ui'})
+
+    assert tags_to_add == [{'Key': 'LabelOwner', 'Value': 'kenisro'},
+                           {'Key': 'LabelProject', 'Value': 'video-monitoring-ui'}]
+
+
+def test_tags_dict_to_list_empty():
+    tags_to_add = Tags.tags_to_update({})
+
+    assert tags_to_add == []
+
+
+test_tags_dict_to_list()
