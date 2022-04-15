2df5e217473ea88a9694e41f31df9354bb28bd32
Vincent Van Gestel <vincent.vangestel@vrt.be>
2022-04-15 14:49:15 +0200

    Add lambda to parse and return json ssm ps entries

---
+++ lambda_code/ssm/ParseDict/index.py
@@ -0,0 +1,50 @@
+"""ParseDict custom resource.
+
+ParseDict custom resource lambda to read json formatted ssm ps parameters
+and return them in easy to work with dict structures.
+"""
+import os
+import json
+
+from cfn_custom_resource import CloudFormationCustomResource
+
+try:
+    from _metadata import CUSTOM_RESOURCE_NAME
+except ImportError:
+    CUSTOM_RESOURCE_NAME = 'dummy'
+
+REGION = os.environ['AWS_REGION']
+
+
+class ParseDict(CloudFormationCustomResource):
+    """
+    ssm.ParseDict.
+
+    Properties:
+        Name: str: Name of the Parameter (including namespace)
+    """
+
+    RESOURCE_TYPE_SPEC = CUSTOM_RESOURCE_NAME
+    # TODO: figure out if we need this or not
+    # DISABLE_PHYSICAL_RESOURCE_ID_GENERATION = True
+
+    def validate(self):
+        self.name = self.resource_properties.get('Name')
+        if not self.name:
+            return False
+        return True
+
+    def create(self):
+        ssm = self.get_boto3_client('ssm')
+        param = ssm.get_parameter(Name=self.name)
+        return json.loads(param['Parameter']['Value'])
+
+    def update(self):
+        return self.create()
+
+    def delete(self):
+        # Nothing to delete
+        pass
+
+
+handler = ParseDict.get_handler()
