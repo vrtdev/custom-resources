09544624deb95184cce16b59f6eab346f2eb7f95
Joris Vandermeersch <joris.vandermeersch@vrt.be>
2025-12-08 20:05:01 +0100

    Improve build.py script

--- build.py
+++ build.py
@@ -6,32 +6,34 @@ list of defined custom resources. For each discovered resource, it creates a
 ZIP-file, and adds the resource to the generated CloudFormation template to
 be deployed.
 """
-import os
+import argparse
 import importlib
+import os
+import shutil
 import subprocess
 import sys
-
-import argparse
-import shutil
 import typing
 import zipfile
 
-try:
-    from pip import main as pipmain  # pip 9
-except ImportError:
-    from pip._internal import main as pipmain  # pip 10
-
 import troposphere
-from troposphere import Template, awslambda, logs, Sub, Output, Export, GetAtt, constants, Ref, Not, Equals, Join, ec2
-from src.custom_resources.LambdaBackedCustomResource import LambdaBackedCustomResource
+from troposphere import Template, awslambda, logs, Output, Export, GetAtt, constants, Ref, Not, Equals, Join, ec2, AWS_STACK_NAME
 
 parser = argparse.ArgumentParser(description='Build custom resources CloudFormation template')
-parser.add_argument('--class-dir', help='Where to look for the CustomResource classes',
-                    default='src/custom_resources')
-parser.add_argument('--lambda-dir', help='Where to look for defined Lambda functions',
-                    default='src/lambda_code')
-parser.add_argument('--output-dir', help='Where to place the Zip-files and the CloudFormation template',
-                    default='output')
+parser.add_argument(
+    '--class-dir',
+    help='Where to look for the CustomResource classes',
+    default='src/custom_resources',
+)
+parser.add_argument(
+    '--lambda-dir',
+    help='Where to look for defined Lambda functions',
+    default='src/lambda_code',
+)
+parser.add_argument(
+    '--output-dir',
+    help='Where to place the Zip-files and the CloudFormation template',
+    default='output',
+)
 
 args = parser.parse_args()
 
@@ -42,12 +44,12 @@ def rec_split_path(path: str) -> typing.List[str]:
 
     Much like os.path.split(), but "recursively".
     """
-    l = []
+    path_components = []
     head = path
     while len(head) > 0:
         head, tail = os.path.split(head)
-        l.insert(0, tail)
-    return l
+        path_components.insert(0, tail)
+    return path_components
 
 
 def rec_join_path(path_list: typing.List[str]) -> str:
@@ -67,29 +69,32 @@ def rec_join_path(path_list: typing.List[str]) -> str:
 
 
 class CustomResource:
+    """CloudFormation CustomResource definition."""
+
     def __init__(
             self,
             name: typing.List[str],
             lambda_path: str,
-            troposphere_class: LambdaBackedCustomResource
+            troposphere_class: type,
     ):
+        """Initialize a CustomResource."""
         self.name = name
         self.lambda_path = lambda_path
         self.troposphere_class = troposphere_class
 
     def __eq__(self, other) -> bool:
+        """Two CustomResources are equal if they have the same troposphere class."""
         if not isinstance(other, self.__class__):
             return False
         return self.troposphere_class == other.troposphere_class
 
     def __hash__(self):
+        """Use the troposphere class as the hash value."""
         return hash(self.troposphere_class)
 
 
 def defined_custom_resources(lambda_dir: str, class_dir: str) -> typing.Set[CustomResource]:
-    """
-    Find custom resources matching our requirements.
-    """
+    """Find custom resources matching our requirements."""
     custom_resources = set()
     for dirpath, dirs, files in os.walk(class_dir):
         for file in files:
@@ -108,7 +113,7 @@ def defined_custom_resources(lambda_dir: str, class_dir: str) -> typing.Set[Cust
             module_location = rec_split_path(fs_path)
             mod = importlib.import_module(
                 '.' + '.'.join(module_location),
-                os.path.basename(class_dir)
+                os.path.basename(class_dir),
             )
 
             for candidate_class_name in dir(mod):
@@ -131,22 +136,26 @@ def defined_custom_resources(lambda_dir: str, class_dir: str) -> typing.Set[Cust
 
 
 def run_pip(*args):
+    """Run pip with the given arguments."""
     subprocess.run(
-        ['pip', *args],
+        ['uv', 'pip', *args],
         check=True,
     )
 
 
 def create_zip_file(custom_resource: CustomResource, output_dir: str):
+    """Create a ZIP file for the given custom resource."""
     dot_joined_resource_name = '.'.join(custom_resource.name)
 
-    print("Creating ZIP for resource {}".format(dot_joined_resource_name))
+    print(f"Creating ZIP for resource {dot_joined_resource_name}")
 
-    zip_filename = "{}.zip".format(dot_joined_resource_name)
+    zip_filename = f"{dot_joined_resource_name}.zip"
     zip_full_filename = os.path.join(output_dir, zip_filename)
-    with zipfile.ZipFile(zip_full_filename,
-                         mode='w',
-                         compression=zipfile.ZIP_DEFLATED) as zip:
+    with zipfile.ZipFile(
+            zip_full_filename,
+            mode='w',
+            compression=zipfile.ZIP_DEFLATED,
+    ) as zipf:
 
         entries = set(os.scandir(custom_resource.lambda_path))
 
@@ -165,22 +174,22 @@ def create_zip_file(custom_resource: CustomResource, output_dir: str):
         if requirements_file is not None:
             # `requirements.txt` found. Interpret it, and add the result to the zip file
             entries.remove(requirements_file)
-            run_pip('install',
-                    '-r', requirements_file.path,
-                    '--isolated',  # Don't automatically add --user (which breaks --target below)
-                                   # --user is on by default on (at least) Debian Buster
-                    '--target', pip_dir)
+            run_pip(
+                'install',
+                '-r', requirements_file.path,
+                '--no-config',  # Don't look for configuration files
+                '--target', pip_dir,
+            )
 
         if test_file is not None:
             entries.remove(test_file)
 
         # Generate _metadata.py file
         with open(os.path.join(pip_dir, "_metadata.py"), "w") as f:
-            f.write("CUSTOM_RESOURCE_NAME = \"{}\"\n".format(
-                custom_resource.troposphere_class.custom_resource_name(
-                    custom_resource.troposphere_class.name()
-                )
-            ))
+            custom_resource_name = custom_resource.troposphere_class.custom_resource_name(
+                custom_resource.troposphere_class.name(),
+            )
+            f.write(f"CUSTOM_RESOURCE_NAME = \"{custom_resource_name}\"\n")
 
         # Add installed/generated files to list to include in ZIP
         entries.update(set(os.scandir(pip_dir)))
@@ -203,11 +212,11 @@ def create_zip_file(custom_resource: CustomResource, output_dir: str):
                 if zip_path.startswith(pip_dir):
                     zip_path = zip_path[(len(pip_dir)+1):]
 
-                zip.write(entry.path, zip_path)
+                zipf.write(entry.path, zip_path)
 
         shutil.rmtree(pip_dir)
 
-    print("ZIP done for resource {}".format(dot_joined_resource_name))
+    print(f"ZIP done for resource {dot_joined_resource_name}")
     print("")
     return zip_filename
 
@@ -220,6 +229,7 @@ except FileExistsError:
 # Import the custom_resources package
 sys.path.insert(0, os.path.dirname(args.class_dir))
 importlib.import_module(os.path.basename(args.class_dir))
+
 
 def create_template(template_name, **kwargs):
     """Create the template."""
@@ -257,15 +267,16 @@ def create_template(template_name, **kwargs):
         # Type cannot be a list of subnets ids if we want them to also support being empty
         Type=constants.COMMA_DELIMITED_LIST,
         Default="",
-        Description="(optional) VPC subnets for Custom Resources that run attached to a VPC"
+        Description="(optional) VPC subnets for Custom Resources that run attached to a VPC",
     ))
     template.set_parameter_label(vpc_subnets, "VPC Subnets")
 
     has_vpc_subnets = template.add_condition("HasVpcSubnets", Not(Equals(Join("", Ref(vpc_subnets)), "")))
 
     def create_custom_resource(custom_resource: CustomResource):
+        dot_joined_resource_name = '.'.join(custom_resource.name)
         custom_resource_name_cfn = custom_resource.troposphere_class.cloudformation_name(
-            custom_resource.troposphere_class.name()
+            custom_resource.troposphere_class.name(),
         )
 
         zip_filename = create_zip_file(custom_resource, args.output_dir)
@@ -276,68 +287,63 @@ def create_template(template_name, **kwargs):
         if "VpcConfig" in function_settings:
             needs_vpc = True
             security_group = template.add_resource(ec2.SecurityGroup(
-                "{custom_resource_name}SecurityGroup".format(custom_resource_name=custom_resource_name_cfn),
-                GroupDescription="Security Group for the {custom_resource_name} custom resource".format(
-                    custom_resource_name='.'.join(custom_resource.name)
-                ),
+                f"{custom_resource_name_cfn}SecurityGroup",
+                GroupDescription=f"Security Group for the {dot_joined_resource_name} custom resource",
                 VpcId=Ref(vpc_id),
             ))
             created_aws_objects.append(security_group)
             created_aws_objects.append(template.add_output(Output(
-                "{custom_resource_name}SecurityGroup".format(custom_resource_name=custom_resource_name_cfn),
+                f"{custom_resource_name_cfn}SecurityGroup",
                 Value=Ref(security_group),
-                Description="Security Group used by the {custom_resource_name} custom resource".format(
-                    custom_resource_name='.'.join(custom_resource.name)
-                ),
-                Export=Export(Sub("${{AWS::StackName}}-{custom_resource_name}SecurityGroup".format(
-                    custom_resource_name=custom_resource_name_cfn,
-                ))),
+                Description=f"Security Group used by the {dot_joined_resource_name} custom resource",
+                Export=Export(Join("-", [
+                    Ref(AWS_STACK_NAME),
+                    f"{custom_resource_name_cfn}SecurityGroup",
+                ])),
             )))
 
             function_settings["VpcConfig"] = awslambda.VPCConfig(
                 SecurityGroupIds=[GetAtt(security_group, 'GroupId')],
-                SubnetIds=Ref(vpc_subnets)
+                SubnetIds=Ref(vpc_subnets),
             )
 
         role = template.add_resource(custom_resource.troposphere_class.lambda_role(
-            "{custom_resource_name}Role".format(custom_resource_name=custom_resource_name_cfn),
+            f"{custom_resource_name_cfn}Role",
         ))
         created_aws_objects.append(role)
         awslambdafunction = template.add_resource(awslambda.Function(
-            "{custom_resource_name}Function".format(custom_resource_name=custom_resource_name_cfn),
+            f"{custom_resource_name_cfn}Function",
             Code=awslambda.Code(
                 S3Bucket=troposphere.Ref(s3_bucket),
                 S3Key=troposphere.Join('', [troposphere.Ref(s3_path),
                                             zip_filename]),
             ),
             Role=GetAtt(role, 'Arn'),
-            **function_settings
+            **function_settings,
         ))
         created_aws_objects.append(awslambdafunction)
         created_aws_objects.append(template.add_resource(logs.LogGroup(
-            "{custom_resource_name}Logs".format(custom_resource_name=custom_resource_name_cfn),
+            f"{custom_resource_name_cfn}Logs",
             LogGroupName=troposphere.Join('', ["/aws/lambda/", troposphere.Ref(awslambdafunction)]),
             RetentionInDays=90,
         )))
         created_aws_objects.append(template.add_output(Output(
-            "{custom_resource_name}ServiceToken".format(custom_resource_name=custom_resource_name_cfn),
+            f"{custom_resource_name_cfn}ServiceToken",
             Value=GetAtt(awslambdafunction, 'Arn'),
-            Description="ServiceToken for the {custom_resource_name} custom resource".format(
-                custom_resource_name='.'.join(custom_resource.name)
-            ),
-            Export=Export(Sub("${{AWS::StackName}}-{custom_resource_name}ServiceToken".format(
-                custom_resource_name=custom_resource_name_cfn
-            )))
+            Description=f"ServiceToken for the {dot_joined_resource_name} custom resource",
+            Export=Export(Join("-", [
+                Ref(AWS_STACK_NAME),
+                f"{custom_resource_name_cfn}ServiceToken",
+            ])),
         )))
         created_aws_objects.append(template.add_output(Output(
-            "{custom_resource_name}Role".format(custom_resource_name=custom_resource_name_cfn),
+            f"{custom_resource_name_cfn}Role",
             Value=GetAtt(role, 'Arn'),
-            Description="Role used by the {custom_resource_name} custom resource".format(
-                custom_resource_name='.'.join(custom_resource.name)
-            ),
-            Export=Export(Sub("${{AWS::StackName}}-{custom_resource_name}Role".format(
-                custom_resource_name=custom_resource_name_cfn,
-            ))),
+            Description=f"Role used by the {dot_joined_resource_name} custom resource",
+            Export=Export(Join("-", [
+                Ref(AWS_STACK_NAME),
+                f"{custom_resource_name_cfn}Role",
+            ])),
         )))
         if needs_vpc:
             for aws_object in created_aws_objects:
